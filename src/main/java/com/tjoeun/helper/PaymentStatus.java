356f208bfe8257d38c055d59ab9fa4752044e1bc
JSH262 <58023108+JSH262@users.noreply.github.com>
2023-08-06 11:07:11 +0900

    Delete src directory

--- src/main/java/com/tjoeun/helper/PaymentStatus.java
+++
@@ -1,7 +0,0 @@
-package com.tjoeun.helper;
-
-public class PaymentStatus {
-	static public final String SUCCESS = "S";
-	static public final String FAIL = "F";
-	static public final String REJECT = "R";	
-}
